37d668b8824a3bc31265daa53d59495ff79e5ec8
Rúben ferreira <56252824+Diamanterub@users.noreply.github.com>
2021-12-30 15:49:33 +0000

    About updated with css

--- js/index.js
+++ js/index.js
@@ -35,15 +35,31 @@
 
     }
 
-    /**
-   * Preloader
-   */
-  let preloader = select('#preloader');
-  if (preloader) {
-    window.addEventListener('load', () => {
-      preloader.remove()
-    });
-  }
 
-  
+    //Preloader
+
+    let preloader = select('#preloader');
+    if (preloader) {
+        window.addEventListener('load', () => {
+            preloader.remove()
+        });
+    }
+
+
+    // Skills animation
+    let skilsContent = select('.skills-content');
+    if (skilsContent) {
+        new Waypoint({
+            element: skilsContent,
+            offset: '80%',
+            handler: function (direction) {
+                let progress = select('.progress .progress-bar', true);
+                progress.forEach((el) => {
+                    el.style.width = el.getAttribute('aria-valuenow') + '%'
+                });
+            }
+        })
+    }
+
+
 })()
