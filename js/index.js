86b35e5406fc9236dee5ff18716bdb83263bdc0f
Rúben ferreira <56252824+Diamanterub@users.noreply.github.com>
2022-01-02 11:29:22 +0000

    Project Selection done,details missing still

--- js/index.js
+++ js/index.js
@@ -7,12 +7,23 @@
 
 
 
+    //Selection functions
     const select = (el, all = false) => {
         el = el.trim()
         if (all) {
             return [...document.querySelectorAll(el)]
         } else {
             return document.querySelector(el)
+        }
+    }
+    const on = (type, el, listener, all = false) => {
+        let selectEl = select(el, all)
+        if (selectEl) {
+            if (all) {
+                selectEl.forEach(e => e.addEventListener(type, listener))
+            } else {
+                selectEl.addEventListener(type, listener)
+            }
         }
     }
 
@@ -62,4 +73,60 @@
     }
 
 
+    //Porfolio isotope and filter
+
+    window.addEventListener('load', () => {
+        let portfolioContainer = select('.portfolio-container');
+        if (portfolioContainer) {
+            let portfolioIsotope = new Isotope(portfolioContainer, {
+                itemSelector: '.portfolio-item'
+            });
+
+            let portfolioFilters = select('#portfolio-flters li', true);
+
+            on('click', '#portfolio-flters li', function (e) {
+                e.preventDefault();
+                portfolioFilters.forEach(function (el) {
+                    el.classList.remove('filter-active');
+                });
+                this.classList.add('filter-active');
+
+                portfolioIsotope.arrange({
+                    filter: this.getAttribute('data-filter')
+                });
+                portfolioIsotope.on('arrangeComplete', function () {
+                    AOS.refresh()
+                });
+            }, true);
+        }
+
+    });
+
+
+
+
 })()
+
+//Function to load the portfolio details only to one page instead of 9
+function portfolioDetailsPageSetup(aplication) {
+
+    //Sending to portfolio detais
+    window.location.assign("portfolio_details.html")
+
+    //Edit content based on the app selected
+
+    // switch(aplication)
+    // {
+
+    //     case 1:
+
+    //         break;
+
+    //         default:
+
+    //             modal.style.display = "none";
+    //             console.log("error")
+    //             alert("error")
+
+    // }
+}
